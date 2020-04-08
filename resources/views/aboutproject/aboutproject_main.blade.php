6f25998d52289df39190fc63e01445d57d15774a
AditS7 <adit.shrestha007@gmail.com>
2020-04-08 21:03:06 +0545

    about project completion

--- resources/views/aboutproject/aboutproject_main.blade.php
+++ resources/views/aboutproject/aboutproject_main.blade.php
@@ -1,1 +1,12 @@
 @extends('main')
+@section('container')
+<div class="main_contain">
+<h2 class="contain_header">About Cinema Management System</h2><hr>
+<p>my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.</p>
+<p>my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.v</p>
+<p>my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.my name is champlord.</p>
+</div>
+@endsection
+@section('footer')
+@include('layouts.footer')
+@endsection
