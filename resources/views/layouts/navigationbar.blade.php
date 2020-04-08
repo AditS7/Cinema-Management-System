9fa6d78464f2d456f738d76cb17eacfac1e86baf
AditS7 <adit.shrestha007@gmail.com>
2020-04-08 15:05:15 +0545

    main page create

---
+++ resources/views/layouts/navigationbar.blade.php
@@ -0,0 +1,8 @@
+<div class="header">
+    <h1 class="header_name">Cinema Management System</h1>
+    <div class="navbar">
+    <a href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    </div>
+</div>
